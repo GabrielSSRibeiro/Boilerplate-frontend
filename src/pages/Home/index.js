772c8bf7bf32465dfda53422daf5fec48ccd8325
Gabriel Ribeiro <49738156+Gab1235@users.noreply.github.com>
2021-05-07 11:34:00 -0400

    added history

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 function Home() {
+  const history = useHistory();
+
   return (
     <div className="Home-container">
       <h1>Hello World</h1>
